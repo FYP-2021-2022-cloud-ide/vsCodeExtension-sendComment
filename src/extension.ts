3b420ec0b4248febe162d055f7900e9d4baa3914
henrywong <yanhowong20@hotmail.com>
2022-03-30 16:35:10 +0800

    markdown

--- src/extension.ts
+++ src/extension.ts
@@ -15,7 +15,7 @@ function sendNotification(title:string, code:string,body:string)
 		grpc.credentials.createInsecure());
 	var docReq = new SendNotificationRequest();
     docReq.setTitle(title)
-    docReq.setBody(code+'\n\n'+"Comment/Question: "+body) 
+    docReq.setBody(code+'\n\n'+"**Comment/Question:** "+body) 
     docReq.setSender(process.env.USER_THIS!)
     docReq.setReceiver(process.env.USER_RESPONSIBLE!)
     docReq.setAllowReply(true)
@@ -124,9 +124,8 @@ export function activate(context: vscode.ExtensionContext) {
 		var assignment = process.env.ASSIGNMENT_NAME!
 		//console.log(thisFilename.toString())
 		var title ="Comment on Assignment "+ assignment+": "
-		var body="in ".concat(thisFilename.toString(),": "
-		+ firstLine+":"+firstChar+"-"+lastLine+":"+lastChar+'\n\n'+
-		"Code: "+'\n'+highlight)
+		var body="in ".concat(thisFilename.toString(),": "+ firstLine+":"+firstChar+"-"+lastLine+":"+lastChar+'\n\n'+
+		"**Code:** \n"+'\n```js'+highlight+"\n```")
 		
 		const userResponse = await vscode.window.showInputBox({
 			placeHolder: 'Type your comment about the selected text...'
